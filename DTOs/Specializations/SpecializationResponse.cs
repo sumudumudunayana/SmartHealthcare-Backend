8e1c402d9e64a85b2c58f618ec548d51a6ee8a31
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-09-20 23:00:30 +0530

    specialization responce updated

---
+++ DTOs/Specializations/SpecializationResponse.cs
@@ -0,0 +1,7 @@
+namespace SmartHealthcare.API.DTOs.Specializations;
+
+public class SpecializationResponse
+{
+    public Guid SpecializationId { get; set; }
+    public string Name { get; set; } = string.Empty;
+}
