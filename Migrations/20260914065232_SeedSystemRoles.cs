e763987e9648bc462146b66741038d286ff6e3fb
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-09-21 18:15:16 +0530

    databse migrations done

---
+++ Migrations/20260914065232_SeedSystemRoles.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace SmartHealthcare.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedSystemRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "RoleId", "RoleName" },
+                values: new object[,]
+                {
+                    { new Guid("11111111-1111-1111-1111-111111111111"), "Administrator" },
+                    { new Guid("22222222-2222-2222-2222-222222222222"), "Doctor" },
+                    { new Guid("33333333-3333-3333-3333-333333333333"), "Receptionist" },
+                    { new Guid("44444444-4444-4444-4444-444444444444"), "Patient" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "RoleId",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111111"));
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "RoleId",
+                keyValue: new Guid("22222222-2222-2222-2222-222222222222"));
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "RoleId",
+                keyValue: new Guid("33333333-3333-3333-3333-333333333333"));
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "RoleId",
+                keyValue: new Guid("44444444-4444-4444-4444-444444444444"));
+        }
+    }
+}
