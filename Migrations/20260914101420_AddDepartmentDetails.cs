4413d5e211db6adb5ea275c27534979863431719
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-09-21 18:15:35 +0530

    databse migrations done

---
+++ Migrations/20260914101420_AddDepartmentDetails.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartHealthcare.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDepartmentDetails : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Departments",
+                type: "character varying(500)",
+                maxLength: 500,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Location",
+                table: "Departments",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Departments");
+
+            migrationBuilder.DropColumn(
+                name: "Location",
+                table: "Departments");
+        }
+    }
+}
